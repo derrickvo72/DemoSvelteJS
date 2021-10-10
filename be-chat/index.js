c3690ba0c58c6fef3895ab44ae5f03db54fd4f8a
DucVP <18110101@student.hcmute.edu.vn>
2021-10-10 10:33:54 +0700

    Svelte-Nodejs-Pusher-ChatApp

---
+++ be-chat/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const cors = require('cors')
+const Pusher = require("pusher");
+
+const pusher = new Pusher({
+  appId: "1273152",
+  key: "dd1d0bbf068b3b713a4e",
+  secret: "798edc7424997b046268",
+  cluster: "ap1",
+  useTLS: true
+});
+
+
+const app = express();
+
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:4200']
+}))
+
+app.use(express.json())
+
+app.post('/api/messages', async (req, res) => {
+    await pusher.trigger("chat", "message", {
+        username: req.body.username,
+        message: req.body.message
+    });
+
+    res.json([]);
+})
+
+console.log('listening to port 8000');
+app.listen(8000)
